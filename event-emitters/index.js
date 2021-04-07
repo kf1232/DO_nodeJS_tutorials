1b0b36a8fef5c5cdd8ef1e03800be49d13b5189b
Kyle Finkernagel <Kyle.Finkernagel@gmail.com>
2021-04-06 19:32:09 -0500

    multiple listeners single event

--- event-emitters/index.js
+++ event-emitters/index.js
@@ -15,7 +15,26 @@ ticketManager.on("error", (error) => {
     console.error(`Gracefully handling our error: ${error}`);
 });
 
-ticketManager.buy("test@email.com", 10);
-ticketManager.buy("test@email.com", 10);
-ticketManager.buy("test@email.com", 10);
-ticketManager.buy("test@email.com", 10);
+console.log(`We have ${ticketManager.listenerCount("buy")} listener(s)for the buy event`);
+console.log(`We have ${ticketManager.listenerCount("error")} listener(s) for the error event`);
+
+const onBuy = () => {
+    console.log("I will be removed soon");
+};
+
+
+ticketManager.on("buy", onBuy);
+
+console.log(`We added a new event listener bringing our total count for the buy event to: ${ticketManager.listenerCount("buy")}`);
+ticketManager.buy("test@email", 20);
+
+
+ticketManager.off("buy", onBuy);
+console.log(`We now have: ${ticketManager.listenerCount("buy")} listener(s) for the buy event`);
+ticketManager.buy("test@email", 20);
+
+
+ticketManager.removeAllListeners("buy");
+console.log(`We have ${ticketManager.listenerCount("buy")} listeners for the buy event`);
+ticketManager.buy("test@email", 20);
+console.log("The last ticket was bought");
